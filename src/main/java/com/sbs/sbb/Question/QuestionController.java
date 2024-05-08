9700e517ba8b71b8a1ddef2bd3b99c47d2d62a92
EoGeunHyeok <eghsamo@naver.com>
2024-05-08 17:46:37 +0900

    답변 등록 버튼

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -3,9 +3,7 @@ package com.sbs.sbb.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RequestMapping("/question") // 앞에 고정적으로 들어가는 단어를 이렇게 지정해주면 편함
@@ -29,4 +27,10 @@ public class QuestionController {
 
         return "question_detail";
     }
+    @PostMapping("/createTemp")
+    @ResponseBody
+    public String temp(){
+        return "전송완료";
+    }
 }
+
