a459d35d23735d53559d0f1e62863b22049b5b5c
EoGeunHyeok <eghsamo@naver.com>
2024-05-08 17:19:00 +0900

    ãexception추가, detail 수정

--- src/main/java/com/sbs/sbb/Question/QuestionService.java
+++ src/main/java/com/sbs/sbb/Question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.sbs.sbb.Question;
 
+import com.sbs.sbb.DataNotException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,6 +14,16 @@ public class QuestionService {
 
     public List<Question> getlist() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> oq = questionRepository.findById(id);
+
+        // oq.isPresent() == false
+        // !oq.isPresent()
+        if( oq.isEmpty()) throw new DataNotException("질문이 없는걸욤");
+
+        return oq.get();
     }
 }
 
